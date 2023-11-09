a86cccaefd9ad3e55cebbae349a86a481a1061e3
EduardoQuijas <143649410+EduardoQuijas@users.noreply.github.com>
2023-11-08 23:32:25 -0600

    Update ExampleException.java

--- ExampleException.java
+++ ExampleException.java
@@ -1,8 +1,6 @@
 package com.docusign.controller.eSignature.examples;
 
-/**
- * Unchecked exception caused by examples.
- */
+
 public class ExampleException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
